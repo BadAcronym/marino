8984d63324f642ea4f7bb993373dcb0451576726
BadAcronym <terribleacronym@gmail.com>
2026-03-08 14:47:10 +0100

    add stub with TODOs

---
+++ src/linux_marino_entry.c
@@ -0,0 +1,7 @@
+// TODO: message loop, etc
+// TODO: figure out metadata file structure, what to do with duplicate game names?
+// TODO: figure out where to store meta-metadata, like which games, where they are, how they're launched, which files they have as metadata
+// TODO: open up a GUI that loads the images with correct titles... like a steam MVP.
+// TODO: steam-like left-panel view
+// TODO: steam big-picture like cover view
+// TODO: compact playnite-like list view with only icons

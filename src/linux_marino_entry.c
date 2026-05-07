9b1e764b86c79e5f60187ffd41247593b4c52a91
BadAcronym <terribleacronym@gmail.com>
2026-05-07 19:33:32 +0200

    hello world

--- src/linux_marino_entry.c
+++ src/linux_marino_entry.c
@@ -1,7 +1,8 @@
-// TODO: message loop, etc
-// TODO: figure out metadata file structure, what to do with duplicate game names?
-// TODO: figure out where to store meta-metadata, like which games, where they are, how they're launched, which files they have as metadata
-// TODO: open up a GUI that loads the images with correct titles... like a steam MVP.
-// TODO: steam-like left-panel view
-// TODO: steam big-picture like cover view
-// TODO: compact playnite-like list view with only icons
+#include "stdio.h"
+
+int main
+(
+    void
+){
+    fprintf(stderr, "TODO: print metadata :D\n");
+}

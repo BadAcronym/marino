48f13a6bd46068c96273095b8f8b2864d66d2c7b
BadAcronym <terribleacronym@gmail.com>
2026-05-31 10:35:44 +0200

    add entry point template

--- src/linux_marino_entry.c
+++ src/linux_marino_entry.c
@@ -1,8 +1,146 @@
-#include "stdio.h"
+#include "river2D_main.h"
+
+#ifdef ASAN
+        #define LIBPATH "./vendor/river2D/bin/asan/"
+#else
+    #ifdef DEBUG
+        #define LIBPATH "./vendor/river2D/bin/debug/"
+    #else
+        #define LIBPATH "./vendor/river2D/bin/release/"
+    #endif
+#endif
 
 int main
 (
     void
 ){
-    fprintf(stderr, "TODO: print metadata :D\n");
+    LauncherData  launcher = {0};
+    EngineData    engine   = {0};
+    River2D_Image planes[RIVER2D_MAX_PLANES] = {0};
+
+    StringView libpath = cstr_sv(LIBPATH);
+    river2D_resolveRenderer(&engine, libpath, RIVER2D_RENDERER_SOFTWARE);
+
+    river2D_loadConfig(&engine.config);
+    engine.config.choices |= RIVER2D_CHOICE_STATIC_CANVAS_BIT;
+    engine.windowName = "River2D Map Editor";
+
+    river2D_init(&engine, planes);
+    marino_init(&engine, &launcher);
+
+    Atom WM_DELETE = XInternAtom(engine.display, "WM_DELETE_WINDOW", false);
+    XSetWMProtocols(engine.display, engine.window, &WM_DELETE, 1);
+
+    bool mapped = false;
+
+    uint32_t resizeWidth  = engine.backbuffer.width;
+    uint32_t resizeHeight = engine.backbuffer.height;
+
+    while(engine.running)
+    {
+        while(XPending(engine.display) > 0)
+        {
+            XEvent event = {0};
+            XNextEvent(engine.display, &event);
+            switch(event.type)
+            {
+                case KeyPress:
+                {
+                    AsciiKey ascii = processXKey(&engine, &event);
+                    marino_processKeys(&engine.controls, ascii, true);
+                    break;
+                }
+                case KeyRelease:
+                {
+                    AsciiKey ascii = processXKey(&engine, &event);
+                    marino_processKeys(&engine.controls, ascii, false);
+                    break;
+                }
+                case ButtonPress:
+                {
+                    if(event.xbutton.button == RIVER2D_MOUSE4)
+                    {
+                        marino_scroll(&launcher, false);
+                        break;
+                    }
+                    else if(event.xbutton.button == RIVER2D_MOUSE5)
+                    {
+                        marino_scroll(&launcher, true);
+                        break;
+                    }
+
+                    marino_processButtons(&launcher, &engine.controls,
+                                           event.xbutton.button, true);
+                    break;
+                }
+                case ButtonRelease:
+                {
+                    marino_processButtons(&launcher, &engine.controls,
+                                           event.xbutton.button, false);
+                    break;
+                }
+                case MotionNotify:
+                {
+                    marino_processPointer(&engine,
+                                           (uint32_t)event.xmotion.x,
+                                           (uint32_t)event.xmotion.y);
+                    break;
+                }
+                case ClientMessage:
+                {
+                    if(event.xclient.data.l[0] == (long)WM_DELETE)
+                    {
+                        engine.running = false;
+                    }
+                    break;
+                }
+                case ConfigureNotify:
+                {
+                    resizeWidth  = (uint32_t)event.xconfigure.width;
+                    resizeHeight = (uint32_t)event.xconfigure.height;
+
+                    if(resizeWidth  != engine.config.window_width  ||
+                       resizeHeight != engine.config.window_height
+                    ){
+                        engine.config.window_width  = resizeWidth;
+                        engine.config.window_height = resizeHeight;
+                    }
+                    break;
+                }
+                case UnmapNotify:
+                {
+                    mapped = false;
+                    break;
+                }
+                case MapNotify:
+                {
+                    mapped  = true;
+                    break;
+                }
+            }
+        }
+
+        if(mapped)
+        {
+            marino_update(&engine, &launcher);
+
+            // 243 -> 240 for the same rounding error reasons as islescape right now :p
+            uint32_t desiredFPS   = 243;
+            int64_t  ns_threshold = (int64_t)(1e9f / (double)(desiredFPS));
+
+            int64_t delta = (int64_t)
+                            river2D_deltaTime_now_ns(&launcher.lastPresentTime);
+
+            if(delta < ns_threshold)
+            {
+                struct timespec duration = {0, ns_threshold - delta};
+                nanosleep(&duration, NULL);
+            }
+
+            marino_present(&engine, &launcher);
+        }
+    }
+
+    river2D_shutdown(&engine);
+    return marino_shutdown(&launcher);
 }
